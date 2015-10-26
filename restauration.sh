9187d937590e6ac115523eeae707340c68f90c94
alexis.c <chalono.alexis@gmail.com>
2015-10-26 16:48:34 +0100

    J'rajoute ça

---
+++ restauration.sh
@@ -0,0 +1,26 @@
+echo LANCEMENT DE LA RESTAURATION
+
+
+
+
+echo Gnome
+sudo apt-get install gnome-tweak-tool
+
+echo Vim
+sudo apt-get install vim
+
+echo Git
+sudo apt-get install git
+
+echo Brackets
+sudo add-apt-repository ppa:webupd8team/brackets
+sudo apt-get update
+sudo apt-get install brackets
+
+echo Shutter
+sudo apt-get install shutter
+
+echo Parcelite
+sudo apt-get install parcellite
+
+
---
+++ serveurweb.sh
@@ -0,0 +1,3 @@
+echo Installation serveur Web
+
+sudo apt-get install apache2 php5 mysql-server libapache2-mod-php5 php5-mysql
